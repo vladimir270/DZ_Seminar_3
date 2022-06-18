15e6787fbebff8a61938dd11da628df6429ecea0
Владимир Ворона <vvorona665@gmail.com>
2022-06-19 01:46:50 +0300

    Completed task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа которая принимает на вход число N и выдает таблицу кубов от 1 до N
+
+Console.WriteLine("Введите число ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+void Cube (int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int Namber2 = i * i * i;
+        Console.Write($"{Namber2},");
+    }
+}
+Cube(Namber);
