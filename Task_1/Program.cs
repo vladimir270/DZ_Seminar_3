e962d18c00fafb91ecec2de40bb18b8082aa46b0
Владимир Ворона <vvorona665@gmail.com>
2022-06-19 00:38:00 +0300

    Completed task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа которая на вход принимает 5 значное число и проверяет, является ли это число палиндромом 
+Console.WriteLine("Введите число ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+void palindrom (int num)
+{
+    int num1 = num / 10000;
+    int num2 = (num % 1000) / 1000;
+    int num4 = (num % 100) / 10;
+    int num5 = (num % 10);
+
+    if (num1 == num5 || num2 == num4)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine("Число НЕ является палиндромом");
+    }
+    
+}
+palindrom(Namber);
